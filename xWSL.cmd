f2170f30c286cc11d7b8d51e0ad613b1ec7db8a4
DesktopECHO <33142753+DesktopECHO@users.noreply.github.com>
2020-09-29 23:51:49 -0300

    Update xWSL.cmd

--- xWSL.cmd
+++ xWSL.cmd
@@ -1,169 +1,60 @@
 @ECHO OFF
-SET GITORG=DesktopECHO
-SET GITPRJ=xWSL
-SET BRANCH=master
-SET BASE=https://github.com/%GITORG%/%GITPRJ%/raw/%BRANCH%
-
-REM ## UAC Check 
-NET SESSION >NUL 2>&1
- if %errorLevel% == 0 (
-      echo Administrative permissions confirmed...
-  ) else (
-      echo You need to run this command with administrative rights.  User Account Control enabled?
-      pause
-      goto ENDSCRIPT
-  )
-
-REM ## Enable WSL
 POWERSHELL.EXE -command "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-CLS && SET RUNSTART=%date% @ %time%
+CLS
+ECHO.Pi-hole 5.x for WSL
+ECHO.------------------------------------------------------------------
+ECHO.Pi-hole listener IP and subnet in CIDR format, ie: 192.168.3.99/24
+SET /p IPSM=Respone: 
+ECHO.
+ECHO.Port for Pi-hole. Port 80 is good if you don't have a webserver, or hit enter for default [8880]: 
+SET PORT=8880& SET /p PORT=Respone: 
+ECHO.
+SET DISTRO=Pi-hole
+IF NOT EXIST %TEMP%\Ubuntu.zip POWERSHELL.EXE -Command "Start-BitsTransfer -source https://aka.ms/wslubuntu2004 -destination %TEMP%\Ubuntu.zip"
+POWERSHELL.EXE -Command "Expand-Archive -Path %TEMP%\Ubuntu.zip -DestinationPath %TEMP% -force
+MKDIR "%PROGRAMFILES%\Pi-hole"
+ECHO.Fetching LxRunOffline...
+POWERSHELL.EXE -Command "wget https://github.com/DesktopECHO/xWSL/raw/master/LxRunOffline.exe -UseBasicParsing -OutFile '%PROGRAMFILES%\Pi-hole\LxRunOffline.exe'"
+SET GO="%PROGRAMFILES%\Pi-hole\LxRunOffline.exe" r -n Pi-hole -c 
+ECHO.
+ECHO.Installing distro...
+START /WAIT /MIN "Install Distro" "%PROGRAMFILES%\Pi-Hole\LxRunOffline.exe" "i" "-n" "Pi-hole" "-f" "%TEMP%\install.tar.gz" -d "%PROGRAMFILES%\Pi-hole" 
+ECHO. 
+ECHO.Configuring distro, this can take a few minutes...
+ECHO. 
+SET GO="%PROGRAMFILES%\Pi-hole\LxRunOffline.exe" r -n Pi-hole -c 
+%GO% "rm -rf /etc/apt/apt.conf.d/20snapd.conf /etc/rc2.d/S01whoopsie /etc/init.d/console-setup.sh ; rm -rf  /var/lib/dpkg/info/udev.postinst"
+%GO% "apt-get -qq update ; apt-get -qq --purge remove openssh-server openssh-sftp-server apport snapd open-iscsi plymouth open-vm-tools mdadm rsyslog ufw irqbalance lvm2 multipath-tools cloud-init cryptsetup cryptsetup-bin cryptsetup-run dbus-user-session dmsetup eject friendly-recovery init libcryptsetup12 libdevmapper1.02.1 libnss-systemd libpam-systemd libparted2 netplan.io packagekit packagekit-tools parted policykit-1 software-properties-common systemd systemd-sysv systemd-timesyncd ubuntu-standard xfsprogs udev --autoremove --allow-remove-essential" > NUL
+%GO% "apt-get -qq install inetutils-syslogd dns-root-data dnsutils gamin idn2 libgamin0 lighttpd netcat php-cgi php-common php-intl php-sqlite3 php-xml php7.4-cgi php7.4-cli php7.4-common php7.4-intl php7.4-json php7.4-opcache php7.4-readline php7.4-sqlite3 php7.4-xml sqlite3 unzip dhcpcd5 nano cron" > NUL
+%GO% "mkdir /etc/pihole ; touch /etc/network/interfaces"
+%GO% "echo BLOCKING_ENABLED=true > /etc/pihole/setupVars.conf"
+%GO% "echo PIHOLE_INTERFACE=eth0 >> /etc/pihole/setupVars.conf"
+%GO% "echo IPV4_ADDRESS=%IPSM% >> /etc/pihole/setupVars.conf"
+%GO% "echo PIHOLE_DNS_1=8.8.8.8 >> /etc/pihole/setupVars.conf"
+%GO% "echo PIHOLE_DNS_2=8.8.4.4 >> /etc/pihole/setupVars.conf"
+%GO% "echo QUERY_LOGGING=true >> /etc/pihole/setupVars.conf"
+%GO% "echo INSTALL_WEB_SERVER=true >> /etc/pihole/setupVars.conf"
+%GO% "echo INSTALL_WEB_INTERFACE=true >> /etc/pihole/setupVars.conf"
+%GO% "echo LIGHTTPD_ENABLED=true >> /etc/pihole/setupVars.conf"
+%GO% "curl -L https://install.Pi-hole.net | bash /dev/stdin --unattended"
+%GO% "echo Web Interface Admin ; pihole -a -p"
+%GO% "sed -i 's/= 80/= %PORT%/g' /etc/lighttpd/lighttpd.conf"
+%GO% "touch /var/run/syslog.pid ; chmod 600 /var/run/syslog.pid"
 
-REM ## Determine ideal DPI
-IF NOT EXIST %TEMP%\dpi.ps1 POWERSHELL.EXE -ExecutionPolicy Bypass -Command "wget %BASE%/dpi.ps1 -UseBasicParsing -OutFile %TEMP%\dpi.ps1"
-FOR /f "delims=" %%a in ('powershell -ExecutionPolicy bypass -command "%TEMP%\dpi.ps1" ') do set "LINDPI=%%a"
+NetSH AdvFirewall Firewall add rule name="WSL Pi-hole Admin Page" dir=in action=allow protocol=TCP localport=%PORT% > NUL
+NetSH AdvFirewall Firewall add rule name="WSL Pi-hole DNS (TCP)" dir=in action=allow protocol=TCP localport=53 > NUL
+NetSH AdvFirewall Firewall add rule name="WSL Pi-hole DNS (UDP)" dir=in action=allow protocol=UDP localport=53 > NUL
 
-REM ## Get installation parameters
-ECHO xWSL Installer
-ECHO:
+SCHTASKS /CREATE /RU %USERNAME% /RL HIGHEST /SC ONSTART /TN "Pi-hole for WSL" /TR '"%PROGRAMFILES%\Pi-hole\Pi-hole_RunTask.cmd"' /F
+ECHO $task = Get-ScheduledTask "Pi-hole for WSL" ; $task.Settings.ExecutionTimeLimit = "PT0S" ; Set-ScheduledTask $task > %TEMP%\ExecTimeLimit.ps1
+POWERSHELL -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -COMMAND %TEMP%\ExecTimeLimit.ps1
 
-:DI
-SET DISTRO=xWSL& SET /p DISTRO=Enter a unique name for the distro or hit Enter to use default [xWSL]: 
-IF EXIST %DISTRO% GOTO DI
-                 SET /p LINDPI=Set custom DPI scale or hit Enter to use Windows value [%LINDPI%]: 
-SET RDPPRT=3399& SET /p RDPPRT=Port number for xRDP traffic or hit Enter to use default [3399]: 
-SET SSHPRT=3322& SET /p SSHPRT=Port number for SSHd traffic or hit Enter to use default [3322]: 
-SET DEFEXL=NONO& SET /p DEFEXL=[Not recommended!] Type X to eXclude %DISTRO% from Windows Defender: 
-
-REM ## Download distro base
-IF /I %CD%==C:\Windows\System32 CD %HOMEPATH%
-SET DISTROFULL=%CD%\%DISTRO%
-SET _rlt=%DISTROFULL:~2,2%
-IF "%_rlt%"=="\\" SET DISTROFULL=%CD%%DISTRO%
-SET GO=%DISTROFULL%\LxRunOffline.exe r -n %DISTRO% -c 
-ECHO: && ECHO %DISTRO% to be installed in %DISTROFULL% && ECHO Downloading... (or using local copy if available)
-IF NOT EXIST %TEMP%\Ubuntu2004.zip POWERSHELL.EXE -Command "Start-BitsTransfer -source https://aka.ms/wslubuntu2004 -destination %TEMP%\Ubuntu2004.zip"
-POWERSHELL.EXE -command "Expand-Archive -Path %TEMP%\Ubuntu2004.zip -DestinationPath %TEMP% -force
-
-REM ## Install Distro with LxRunOffline / https://github.com/DDoSolitary/LxRunOffline
-IF NOT EXIST %TEMP%\LxRunOffline.exe POWERSHELL.EXE -Command "wget %BASE%/LxRunOffline.exe -UseBasicParsing -OutFile %TEMP%\LxRunOffline.exe"
-%TEMP%\LxRunOffline.exe  i -n %DISTRO% -d .\%DISTRO% -f %TEMP%\install.tar.gz
-%TEMP%\LxRunOffline.exe sd -n %DISTRO%
-COPY %TEMP%\LxRunOffline.* %DISTROFULL% > NUL
-
-REM ## Add exclusions in Windows Defender if requested
-IF NOT EXIST %TEMP%\excludeWSL.ps1 POWERSHELL.EXE -Command "wget %BASE%/excludeWSL.ps1 -UseBasicParsing -OutFile %TEMP%\excludeWSL.ps1"
-IF %DEFEXL%==X POWERSHELL.EXE -ExecutionPolicy bypass -command "%TEMP%\excludeWSL.ps1 '%DISTROFULL%'"
-
-REM ## The following line will be removed when the 2020-08 WSL update shows up in WU for non-seekers 
-%GO% "add-apt-repository -y ppa:rafaeldtinoco/lp1871129 ; apt install libc6=2.31-0ubuntu8+lp1871129~1 libc6-dev=2.31-0ubuntu8+lp1871129~1 libc-bin=2.31-0ubuntu8+lp1871129~1 libc-dev-bin=2.31-0ubuntu8+lp1871129~1 -y --allow-downgrades ; apt-mark hold libc6"
-
-REM ## Download xWSL overlay
-CD %DISTROFULL%
-%GO% "cd /tmp ; git clone -b %BRANCH% --depth=1 https://github.com/%GITORG%/%GITPRJ%.git"
-%GO% "ssh-keygen -A ; mkdir -p /root/.local/share ; apt-get update"
-
-REM ## Install local packages
-%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install /tmp/xWSL/deb/gksu_2.1.0_amd64.deb /tmp/xWSL/deb/libgksu2-0_2.1.0_amd64.deb /tmp/xWSL/deb/libgnome-keyring0_3.12.0-1+b2_amd64.deb /tmp/xWSL/deb/libgnome-keyring-common_3.12.0-1_all.deb /tmp/xWSL/deb/multiarch-support_2.27-3ubuntu1_amd64.deb /tmp/xWSL/deb/xrdp_0.9.13.1-2_amd64.deb /tmp/xWSL/deb/xorgxrdp_0.2.12-1_amd64.deb /tmp/xWSL/deb/plata-theme_0.9.8-0ubuntu1~focal1_all.deb /tmp/xWSL/deb/papirus-icon-theme_20200901-4672+pkg21~ubuntu20.04.1_all.deb /tmp/xWSL/deb/fonts-cascadia-code_2005.15-1_all.deb --no-install-recommends"
-
-REM ## Install Seamonkey Browser
-%GO% "echo deb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main >> /etc/apt/sources.list"
-%GO% "apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2667CA5C ; apt-get update ; apt-get -y install seamonkey-mozilla-build"
-%GO% "update-alternatives --install /usr/bin/www-browser www-browser /usr/bin/seamonkey 100 ; update-alternatives --install /usr/bin/gnome-www-browser gnome-www-browser /usr/bin/seamonkey 100 ; update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/seamonkey 100"
-
-REM ## Install dependencies for desktop environments
-%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install x11-apps x11-session-utils x11-xserver-utils pulseaudio dialog distro-info-data lsb-release dumb-init inetutils-syslogd xdg-utils avahi-daemon libnss-mdns binutils putty synaptic pulseaudio-utils pulseaudio mesa-utils bzip2 p7zip-full unar unzip zip libatkmm-1.6-1v5 libcairomm-1.0-1v5 libcanberra-gtk3-0 libcanberra-gtk3-module libglibmm-2.4-1v5 libgtkmm-3.0-1v5 libpangomm-1.4-1v5 libsigc++-2.0-0v5 dbus-x11 libdbus-glib-1-2 libqt5core5a --no-install-recommends"
-
-REM ## Install XFCE4
-%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4-terminal xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin pavucontrol xfwm4 xfce4-panel xfce4-session xfce4-settings thunar thunar-volman thunar-archive-plugin xfdesktop4 xfce4-screenshooter libsmbclient gigolo gvfs-fuse gvfs-backends gvfs-bin mousepad evince xarchiver lhasa lrzip lzip lzop ncompress zip unzip dmz-cursor-theme adapta-gtk-theme gconf-defaults-service xfce4-taskmanager hardinfo --no-install-recommends" 
-
-REM ## Install Media Player and Image Editor
-%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install mtpaint parole"
-
-REM ## Additional items to install can go here...
-REM ## %GO% "cd /tmp ; wget https://files.multimc.org/downloads/multimc_1.4-1.deb"
-REM ## %GO% "apt-get -y install extremetuxracer tilix /tmp/multimc_1.4-1.deb"
-
-REM ## Remove un-needed packages
-%GO% "apt-get -qq purge cryptsetup cryptsetup-bin cryptsetup-initramfs cryptsetup-run irqbalance multipath-tools apparmor snapd squashfs-tools libplymouth5 plymouth plymouth-theme-ubuntu-text open-vm-tools cloud-init isc-dhcp-* gnustep* lvm2* mdadm apport open-iscsi powermgmt-base popularity-contest fwupd libfwupd2 ; apt-get -qq autoremove ; apt-get -qq clean" > NUL
-
-REM ## Customize
-IF %LINDPI% GEQ 288 ( %GO% "sed -i 's/HISCALE/3/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml" )
-IF %LINDPI% GEQ 192 ( %GO% "sed -i 's/HISCALE/2/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml" )
-IF %LINDPI% GEQ 192 ( %GO% "sed -i 's/Default-hdpi/Default-xhdpi/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml" )
-IF %LINDPI% GEQ 192 ( %GO% "sed -i 's/Segoe UI Semi-Bold 11/Segoe UI Semi-Bold 22/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml" )
-IF %LINDPI% GEQ 192 ( %GO% "sed -i 's/QQQ/96/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml" )
-IF %LINDPI% LSS 192 ( %GO% "sed -i 's/QQQ/%LINDPI%/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml" )
-IF %LINDPI% LSS 192 ( %GO% "sed -i 's/HISCALE/1/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml" )
-IF %LINDPI% LSS 120 ( %GO% "sed -i 's/Default-hdpi/Default/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml" )
-SET /A SESMAN = %RDPPRT% - 50
-%GO% "sed -i 's/ListenPort=3350/ListenPort=%SESMAN%/g' /etc/xrdp/sesman.ini"
-%GO% "sed -i 's/thinclient_drives/.xWSL/g' /etc/xrdp/sesman.ini"
-%GO% "sed -i 's/port=3389/port=%RDPPRT%/g' /tmp/xWSL/dist/etc/xrdp/xrdp.ini ; cp /tmp/xWSL/dist/etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini"
-%GO% "sed -i 's/#Port 22/Port %SSHPRT%/g' /etc/ssh/sshd_config"
-%GO% "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
-%GO% "sed -i 's/XWSLINSTANCENAME/%DISTRO%/g' /tmp/xWSL/dist/usr/local/bin/initWSL"
-%GO% "sed -i 's/#enable-dbus=yes/enable-dbus=no/g' /etc/avahi/avahi-daemon.conf ; sed -i 's/#host-name=foo/host-name=%COMPUTERNAME%-%DISTRO%/g' /etc/avahi/avahi-daemon.conf"
-%GO% "cp /mnt/c/Windows/Fonts/*.ttf /usr/share/fonts/truetype ; rm -rf /etc/pam.d/systemd-user ; rm -rf /etc/systemd ; rm -rf /usr/share/icons/breeze_cursors ; rm -rf /usr/share/icons/Breeze_Snow/cursors ; ssh-keygen -A ; adduser xrdp ssl-cert"
-%GO% "mv /usr/bin/pkexec /usr/bin/pkexec.orig ; echo gksudo -k -S -g \$1 > /usr/bin/pkexec ; chmod 755 /usr/bin/pkexec"
-%GO% "chmod 644 /tmp/xWSL/dist/etc/wsl.conf"
-%GO% "chmod 644 /tmp/xWSL/dist/var/lib/xrdp-pulseaudio-installer/*.so"
-%GO% "chmod 700 /tmp/xWSL/dist/usr/local/bin/initWSL ; chmod 700 /tmp/xWSL/dist/etc/skel/.config ; chmod 700 /tmp/xWSL/dist/etc/skel/.local ; chmod 700 /tmp/xWSL/dist/etc/skel/.gconf ; chmod 700 /tmp/xWSL/dist/etc/skel/.mozilla"
-%GO% "chmod 644 /tmp/xWSL/dist/etc/profile.d/WinNT.sh"
-%GO% "chmod 644 /tmp/xWSL/dist/etc/xrdp/xrdp.ini"
-%GO% "cp -r /tmp/xWSL/dist/* /"
-%GO% "rm -rf /etc/apt/apt.conf.d/20snapd.conf /etc/rc2.d/S01whoopsie /etc/init.d/console-setup.sh"
-%GO% "strip --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt5Core.so.5"
-
-REM ## Setup user access 
-SET RUNEND=%date% @ %time%
-CD %DISTROFULL% 
-ECHO:
-ECHO:
-SET /p XU=Enter name of %DISTRO% user: 
-BASH -c "useradd -m -p nulltemp -s /bin/bash %XU%"
-POWERSHELL -Command $prd = read-host "Enter password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($prd) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp & set /p PWO=<.tmp
-BASH -c "echo %XU%:%PWO% | chpasswd"
-%GO% "sed -i 's/PLACEHOLDER/%XU%/g' /tmp/xWSL/xWSL.rdp"
-%GO% "sed -i 's/COMPY/%COMPUTERNAME%/g' /tmp/xWSL/xWSL.rdp"
-%GO% "sed -i 's/RDPPRT/%RDPPRT%/g' /tmp/xWSL/xWSL.rdp"
-%GO% "cp /tmp/xWSL/xWSL.rdp ./xWSL._"
-ECHO $prd = Get-Content .tmp > .tmp.ps1
-ECHO ($prd ^| ConvertTo-SecureString -AsPlainText -Force) ^| ConvertFrom-SecureString ^| Out-File .tmp  >> .tmp.ps1
-POWERSHELL -ExecutionPolicy Bypass -Command .tmp.ps1 
-TYPE .tmp>.tmpsec.txt
-COPY /y /b %DISTROFULL%\xWSL._+.tmpsec.txt "%DISTROFULL%\%DISTRO% (%XU%) Desktop.rdp" > NUL
-DEL /Q  xWSL._ .tmp*.* > NUL
-BASH -c "echo '%XU% ALL=(ALL:ALL) ALL' >> /etc/sudoers"
-
-REM ## Open Firewall Ports
-NETSH AdvFirewall Firewall add rule name="%DISTRO% xRDP" dir=in action=allow protocol=TCP localport=%RDPPRT% > NUL
-NETSH AdvFirewall Firewall add rule name="%DISTRO% Secure Shell" dir=in action=allow protocol=TCP localport=%SSHPRT% > NUL
-NETSH AdvFirewall Firewall add rule name="%DISTRO% Avahi Multicast DNS" dir=in action=allow program="%DISTROFULL%\rootfs\usr\sbin\avahi-daemon" enable=yes > NUL
-
-REM ## Build RDP, Console, Init Links, Scheduled Task...
-ECHO @WSLCONFIG /t %DISTRO% > "%DISTROFULL%\Init.cmd"
-ECHO @WSL ~ -u root -d %DISTRO% -e initWSL 2 >> "%DISTROFULL%\Init.cmd"
-ECHO @WSL ~ -u %XU% -d %DISTRO% >  "%DISTROFULL%\%DISTRO% (%XU%) Console.cmd"
-COPY /Y "%DISTROFULL%\%DISTRO% (%XU%) Console.cmd" "%USERPROFILE%\Desktop\%DISTRO% (%XU%) Console.cmd" > NUL
-COPY /Y "%DISTROFULL%\%DISTRO% (%XU%) Desktop.rdp" "%USERPROFILE%\Desktop\%DISTRO% (%XU%) Desktop.rdp" > NUL
-START /MIN "%DISTRO% Init" WSL ~ -u root -d %DISTRO% -e initWSL 2
-POWERSHELL -C "$WAI = (whoami) ; (Get-Content .\rootfs\tmp\xWSL\xWSL.xml).replace('AAAA', $WAI) | Set-Content .\rootfs\tmp\xWSL\xWSL.xml"
-POWERSHELL -C "$WAC = (pwd)    ; (Get-Content .\rootfs\tmp\xWSL\xWSL.xml).replace('QQQQ', $WAC) | Set-Content .\rootfs\tmp\xWSL\xWSL.xml"
-SCHTASKS /Create /TN:%distro% /XML .\rootfs\tmp\xWSL\xWSL.xml /F
-ECHO:
-ECHO:      Start: %RUNSTART%
-ECHO:        End: %RUNEND%
-%GO%  "echo -ne '   Packages:'\   ; dpkg-query -l | grep "^ii" | wc -l "
-ECHO: 
-ECHO:  - xRDP Server listening on port %RDPPRT% and SSHd on port %SSHPRT%.
-ECHO: 
-ECHO:  - Links for GUI and Console sessions have been placed on your desktop.
-ECHO: 
-ECHO:  - (Re)launch init from the Task Scheduler or by running the following command: 
-ECHO:    schtasks /run /tn %DISTRO%
-ECHO: 
-ECHO: %DISTRO% Installation Complete!  GUI will start in a few seconds...  
-PING -n 6 LOCALHOST > NUL 
-START "Remote Desktop Connection" "MSTSC.EXE" "/V" "%DISTROFULL%\%DISTRO% (%XU%) Desktop.rdp"
-ECHO: 
-:ENDSCRIPT
+ECHO @"%PROGRAMFILES%\Pi-hole\LxRunOffline.exe" r -n Pi-hole -c "pihole -r" > "%PROGRAMFILES%\Pi-hole\Pi-hole_ResetReconfigure.cmd"
+ECHO @"%PROGRAMFILES%\Pi-Hole\LxRunOffline.exe" r -n Pi-hole -c "apt-get -qq remove dhcpcd5" > "%PROGRAMFILES%\Pi-hole\Pi-hole_RunTask.cmd" 
+ECHO @"%PROGRAMFILES%\Pi-Hole\LxRunOffline.exe" r -n Pi-hole -c "sed -i 's/= 80/= %PORT%/g'  /etc/lighttpd/lighttpd.conf" >> "%PROGRAMFILES%\Pi-hole\Pi-hole_RunTask.cmd"
+ECHO @%GO% "for rc_service in /etc/rc2.d/S*; do [[ -e $rc_service ]] && $rc_service restart ; done" >> "%PROGRAMFILES%\Pi-hole\Pi-hole_RunTask.cmd"
+SCHTASKS /RUN /TN "Pi-hole for WSL"
+ECHO Wait for launcher window to close then
+PAUSE
+ECHO Pi-hole 5.x for WSL Install Complete!
+START http://%COMPUTERNAME%:%PORT%/admin

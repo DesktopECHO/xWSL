71c4c5600c32874ac3f71c6e249ec7467e102aa4
DesktopECHO <33142753+DesktopECHO@users.noreply.github.com>
2020-12-14 12:16:12 -0400

    Update xWSL.cmd

--- xWSL.cmd
+++ xWSL.cmd
@@ -3,7 +3,7 @@ IF %ERRORLEVEL% == 0 (ECHO Administrator check passed...) ELSE (ECHO You need to
 COLOR 1F
 SET GITORG=DesktopECHO
 SET GITPRJ=xWSL
-SET BRANCH=experimental
+SET BRANCH=master
 SET BASE=https://github.com/%GITORG%/%GITPRJ%/raw/%BRANCH%
 
 REM ## Enable WSL if required

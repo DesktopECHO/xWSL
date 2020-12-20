34ac47ab2bc904b91df4a1317f4846fca7052264
DesktopECHO <33142753+DesktopECHO@users.noreply.github.com>
2020-12-19 21:20:23 -0400

    Fix crash on older CPUs

--- xWSL.cmd
+++ xWSL.cmd
@@ -103,7 +103,7 @@ REM ## %GO% "cd /tmp ; wget https://files.multimc.org/downloads/multimc_1.4-1.de
 REM ## %GO% "apt-get -y install supertuxkart /tmp/multimc_1.4-1.deb"
 
 ECHO [%TIME:~0,8%] Post-install clean-up (~0m45s)
-%GO% "apt-get -y purge libxslt1.1 gnustep-base-runtime libgnustep-base1.26 gnustep-base-common gnustep-common libgc1c2 libobjc4 powermgmt-base unar ; apt-get -y clean" > ".\logs\%TIME:~0,2%%TIME:~3,2%%TIME:~6,2% Post-install clean-up.log"
+%GO% "apt-get -y purge mesa-vulkan-drivers libxslt1.1 gnustep-base-runtime libgnustep-base1.26 gnustep-base-common gnustep-common libgc1c2 libobjc4 powermgmt-base unar ; apt-get -y clean" > ".\logs\%TIME:~0,2%%TIME:~3,2%%TIME:~6,2% Post-install clean-up.log"
 
 SET /A SESMAN = %RDPPRT% - 50
 %GO% "which schtasks.exe" > "%TEMP%\SCHT.tmp" & set /p SCHT=<"%TEMP%\SCHT.tmp"

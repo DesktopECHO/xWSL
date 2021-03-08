755a9684e59f88cd4d0b8dab64dff41e9b3fd92b
DesktopECHO <33142753+DesktopECHO@users.noreply.github.com>
2021-03-08 10:11:52 -0400

    Revert "Update"
    This reverts commit 73f9e90e7ad2b0d72c49a4fc323a88d2b7642fae.

--- dist/etc/profile.d/xWSL.sh
+++ dist/etc/profile.d/xWSL.sh
@@ -16,16 +16,14 @@ if [ -z "$XDG_DATA_DIRS" ]; then
     export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 fi
 
-unset LIBGL_ALWAYS_INDIRECT
 export GTK2_RC_FILES=$HOME/.config/gtkrc-2.0
 export QT_QPA_PLATFORMTHEME=gtk2
 export QT_X11_NO_MITSHM=1
 export QT_ACCESSIBILITY=0
-export NO_AT_BRIDGE=1
 export MOZ_FORCE_DISABLE_E10S=1
 export MOZ_LAYERS_ALLOW_SOFTWARE_GL=1
+export LIBXCB_ALLOW_SLOPPY_LOCK=1
 export DESKTOP_SESSION=xfce
-export WEBKIT_DISABLE_COMPOSITING_MODE=1
 export XDG_SESSION_TYPE=x11
 export XDG_SESSION_DESKTOP=xfce
 export XDG_CURRENT_DESKTOP=XFCE
@@ -33,5 +31,5 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_RUNTIME_DIR=$HOME/.local
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
-
-
+export NO_AT_BRIDGE=1
+export LIBGL_ALWAYS_INDIRECT=0
